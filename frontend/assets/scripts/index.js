34309dd3c3f1858a3ef5f0d9dd98cba77aeb815f
iwamoto <iwamoto@cshool.jp>
2018-08-17 13:00:09 +0900

    js修正

--- frontend/assets/scripts/index.js
+++ frontend/assets/scripts/index.js
@@ -3,5 +3,5 @@ import $ from 'jquery';
 import '../styles/index.scss'; // webpackでコンパイルするため必須. 実際には.jsには展開されない.
 
 $(document).ready(() => {
-  window.console.log('hello!?');
+  window.console.log('hello!');
 });
